e2dd2bdb8f3ab9630c0bfa9a1d088247a724c980
Manuel Sierra <sierramorenomanuel97@gmail.com>
2023-06-14 03:34:41 -0500

    regresando  a posicion inicial el seccion de proyectos

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//regresando  a posicion inicial el seccion de proyectos
+const header = document
+  .querySelector("header")
+  .addEventListener("mouseover", () => {
+    const checkbox = (document.getElementById("checkProyects").checked = false);
+  });
