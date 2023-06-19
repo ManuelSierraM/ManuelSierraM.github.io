402139f09df56df03fc2f787543d7e5093c340a8
Manuel Sierra <sierramorenomanuel97@gmail.com>
2023-06-19 00:22:21 -0500

    implementacion de scroll snap en el cuerpo de la pagina

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //regresando  a posicion inicial el seccion de proyectos
-const header = document
-  .querySelector("header")
+const headerArticle_section = document
+  .getElementById("headerArticle")
   .addEventListener("mouseover", () => {
     const checkbox = (document.getElementById("checkProyects").checked = false);
   });
